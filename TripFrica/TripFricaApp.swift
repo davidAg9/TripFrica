c8e3b759f6131a89dee011f131a65da6cea4b3e4
davidAg9 <shane.qoubby@gmail.com>
2021-01-01 07:36:35 +0000

    Initial Commit

---
+++ TripFrica/TripFricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripFricaApp.swift
+//  TripFrica
+//
+//  Created by Dave Agyakwa on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TripFricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
